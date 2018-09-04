18dbf04cf492179f3aea292cb1792a9cbf38a487
colinporth <colinporth@users.noreply.github.com>
2018-09-04 09:53:11 +0100

    fiddle

---
+++ nucleo/main.cpp
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int i;
+
+  for (i = 0; i < 100; i++) {
+    printf("Hello World %d!\n", i);
+  }
+  do {
+    i++;
+  } while (1);
+
+return 0;
+}
