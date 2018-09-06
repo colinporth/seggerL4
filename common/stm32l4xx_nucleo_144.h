db55e102852f352d7011d1ef30f0b43d6f1a2598
colinporth <colinporth@users.noreply.github.com>
2018-09-06 17:00:22 +0100

    fiddle

--- common/stm32l4xx_nucleo_144.h
+++ common/stm32l4xx_nucleo_144.h
@@ -11,9 +11,7 @@ typedef enum { BUTTON_USER = 0, /* Alias */ BUTTON_KEY = BUTTON_USER } Button_Ty
 typedef enum { BUTTON_MODE_GPIO = 0, BUTTON_MODE_EXTI = 1 } ButtonMode_TypeDef;
 typedef enum { JOY_NONE = 0, JOY_SEL = 1, JOY_DOWN = 2, JOY_LEFT = 3, JOY_RIGHT = 4, JOY_UP = 5 } JOYState_TypeDef;
 
-#if !defined (USE_STM32L4XX_NUCLEO_144)
-  #define USE_STM32L4XX_NUCLEO_144
-#endif
+#define USE_STM32L4XX_NUCLEO_144
 
 //{{{  leds
 #define LEDn                                    3
@@ -70,82 +68,9 @@ typedef enum { JOY_NONE = 0, JOY_SEL = 1, JOY_DOWN = 2, JOY_LEFT = 3, JOY_RIGHT 
 //}}}
 
 #ifdef USE_STM32L4XX_NUCLEO_144_SMPS
-  #define SMPS_OK     0
-  #define SMPS_KO     1
-#endif 
-
-#ifdef HAL_SPI_MODULE_ENABLED
-  //{{{  spi
-  #define NUCLEO_SPIx                                     SPI1
-  #define NUCLEO_SPIx_CLK_ENABLE()                        __HAL_RCC_SPI1_CLK_ENABLE()
-
-  #define NUCLEO_SPIx_SCK_AF                              GPIO_AF5_SPI1
-  #define NUCLEO_SPIx_SCK_GPIO_PORT                       GPIOA
-  #define NUCLEO_SPIx_SCK_PIN                             GPIO_PIN_5
-  #define NUCLEO_SPIx_SCK_GPIO_CLK_ENABLE()               __HAL_RCC_GPIOA_CLK_ENABLE()
-  #define NUCLEO_SPIx_SCK_GPIO_CLK_DISABLE()              __HAL_RCC_GPIOA_CLK_DISABLE()
-
-  #define NUCLEO_SPIx_MISO_MOSI_AF                        GPIO_AF5_SPI1
-  #define NUCLEO_SPIx_MISO_MOSI_GPIO_PORT                 GPIOA
-  #define NUCLEO_SPIx_MISO_MOSI_GPIO_CLK_ENABLE()         __HAL_RCC_GPIOA_CLK_ENABLE()
-  #define NUCLEO_SPIx_MISO_MOSI_GPIO_CLK_DISABLE()        __HAL_RCC_GPIOA_CLK_DISABLE()
-  #define NUCLEO_SPIx_MISO_PIN                            GPIO_PIN_6
-  #define NUCLEO_SPIx_MOSI_PIN                            GPIO_PIN_7
-  /* Maximum Timeout values for flags waiting loops. These timeouts are not based
-     on accurate values, they just guarantee that the application will not remain
-     stuck if the SPI communication is corrupted.
-     You may modify these timeout values depending on CPU frequency and application
-     conditions (interrupts routines ...). */
-  #define NUCLEO_SPIx_TIMEOUT_MAX                   1000
-
-  #define NUCLEO_SPIx_CS_GPIO_PORT                        GPIOD
-  #define NUCLEO_SPIx_CS_PIN                              GPIO_PIN_14
-  #define NUCLEO_SPIx_CS_GPIO_CLK_ENABLE()                __HAL_RCC_GPIOD_CLK_ENABLE()
-  #define NUCLEO_SPIx_CS_GPIO_CLK_DISABLE()               __HAL_RCC_GPIOD_CLK_DISABLE()
-
-  #define SPIx__CS_LOW()       HAL_GPIO_WritePin(NUCLEO_SPIx_CS_GPIO_PORT, NUCLEO_SPIx_CS_PIN, GPIO_PIN_RESET)
-  #define SPIx__CS_HIGH()      HAL_GPIO_WritePin(NUCLEO_SPIx_CS_GPIO_PORT, NUCLEO_SPIx_CS_PIN, GPIO_PIN_SET)
-
-  #define SD_CS_LOW()       HAL_GPIO_WritePin(SD_CS_GPIO_PORT, SD_CS_PIN, GPIO_PIN_RESET)
-  #define SD_CS_HIGH()      HAL_GPIO_WritePin(SD_CS_GPIO_PORT, SD_CS_PIN, GPIO_PIN_SET)
-
-  #define LCD_CS_LOW()      HAL_GPIO_WritePin(LCD_CS_GPIO_PORT, LCD_CS_PIN, GPIO_PIN_RESET)
-  #define LCD_CS_HIGH()     HAL_GPIO_WritePin(LCD_CS_GPIO_PORT, LCD_CS_PIN, GPIO_PIN_SET)
-  #define LCD_DC_LOW()      HAL_GPIO_WritePin(LCD_DC_GPIO_PORT, LCD_DC_PIN, GPIO_PIN_RESET)
-  #define LCD_DC_HIGH()     HAL_GPIO_WritePin(LCD_DC_GPIO_PORT, LCD_DC_PIN, GPIO_PIN_SET)
-
-  #define SD_CS_PIN                                 GPIO_PIN_14
-  #define SD_CS_GPIO_PORT                           GPIOF
-  #define SD_CS_GPIO_CLK_ENABLE()                 __HAL_RCC_GPIOF_CLK_ENABLE()
-  #define SD_CS_GPIO_CLK_DISABLE()                __HAL_RCC_GPIOF_CLK_DISABLE()
-
-  #define LCD_CS_PIN                                 GPIO_PIN_14
-  #define LCD_CS_GPIO_PORT                           GPIOD
-  #define LCD_CS_GPIO_CLK_ENABLE()                 __HAL_RCC_GPIOD_CLK_ENABLE()
-  #define LCD_CS_GPIO_CLK_DISABLE()                __HAL_RCC_GPIOD_CLK_DISABLE()
-
-  #define LCD_DC_PIN                                 GPIO_PIN_12
-  #define LCD_DC_GPIO_PORT                           GPIOF
-  #define LCD_DC_GPIO_CLK_ENABLE()                 __HAL_RCC_GPIOF_CLK_ENABLE()
-  #define LCD_DC_GPIO_CLK_DISABLE()                __HAL_RCC_GPIOF_CLK_DISABLE()
-  //}}}
-#endif 
-
-#ifdef HAL_ADC_MODULE_ENABLED
-  //{{{  adc
-  #define NUCLEO_ADCx                               ADC1
-
-  #define NUCLEO_ADCx_CHANNEL                       ADC_CHANNEL_2
-  #define NUCLEO_ADCx_SAMPLETIME                    ADC_SAMPLETIME_24CYCLES_5
-  #define NUCLEO_ADCx_CLK_ENABLE()                  __HAL_RCC_ADC_CLK_ENABLE()
-  #define NUCLEO_ADCx_CLK_DISABLE()                 __HAL_RCC_ADC_CLK_DISABLE()
-
-  #define NUCLEO_ADCx_GPIO_PORT                       GPIOC
-  #define NUCLEO_ADCx_GPIO_PIN                        GPIO_PIN_1
-  #define NUCLEO_ADCx_GPIO_CLK_ENABLE()             __HAL_RCC_GPIOC_CLK_ENABLE()
-  #define NUCLEO_ADCx_GPIO_CLK_DISABLE()            __HAL_RCC_GPIOC_CLK_DISABLE()
-  //}}}
-#endif 
+  #define SMPS_OK  0
+  #define SMPS_KO  1
+#endif
 
 #ifdef USE_STM32L4XX_NUCLEO_144_SMPS
   #ifdef USE_ADP5301ACBZ
@@ -166,32 +91,24 @@ typedef enum { JOY_NONE = 0, JOY_SEL = 1, JOY_DOWN = 2, JOY_LEFT = 3, JOY_RIGHT 
   #endif
 #endif
 
-uint32_t         BSP_GetVersion(void);
+void BSP_LED_Init (Led_TypeDef Led);
+void BSP_LED_DeInit (Led_TypeDef Led);
+void BSP_LED_On (Led_TypeDef Led);
+void BSP_LED_Off (Led_TypeDef Led);
+void BSP_LED_Toggle (Led_TypeDef Led);
 
-void             BSP_LED_Init(Led_TypeDef Led);
-void             BSP_LED_DeInit(Led_TypeDef Led);
-void             BSP_LED_On(Led_TypeDef Led);
-void             BSP_LED_Off(Led_TypeDef Led);
-void             BSP_LED_Toggle(Led_TypeDef Led);
-
-void             BSP_PB_Init(Button_TypeDef Button, ButtonMode_TypeDef ButtonMode);
-void             BSP_PB_DeInit(Button_TypeDef Button);
-uint32_t         BSP_PB_GetState(Button_TypeDef Button);
-
-#if defined(HAL_ADC_MODULE_ENABLED)
-  uint8_t          BSP_JOY_Init(void);
-  void             BSP_JOY_DeInit(void);
-  JOYState_TypeDef BSP_JOY_GetState(void);
-#endif
+void BSP_PB_Init (Button_TypeDef Button, ButtonMode_TypeDef ButtonMode);
+void BSP_PB_DeInit (Button_TypeDef Button);
+uint32_t BSP_PB_GetState (Button_TypeDef Button);
 
 #ifdef USE_STM32L4XX_NUCLEO_144_SMPS
-  uint32_t         BSP_SMPS_Init(uint32_t VoltageRange);
-  uint32_t         BSP_SMPS_DeInit(void);
-  uint32_t         BSP_SMPS_Enable(uint32_t Delay, uint32_t Power_Good_Check);
-  uint32_t         BSP_SMPS_Disable(void);
-  uint32_t         BSP_SMPS_Supply_Enable(uint32_t Delay, uint32_t Power_Good_Check);
-  uint32_t         BSP_SMPS_Supply_Disable(void);
-#endif 
+  uint32_t BSP_SMPS_Init (uint32_t VoltageRange);
+  uint32_t BSP_SMPS_DeInit();
+  uint32_t BSP_SMPS_Enable (uint32_t Delay, uint32_t Power_Good_Check);
+  uint32_t BSP_SMPS_Disable();
+  uint32_t BSP_SMPS_Supply_Enable (uint32_t Delay, uint32_t Power_Good_Check);
+  uint32_t BSP_SMPS_Supply_Disable();
+#endif
 
 //{{{
 #ifdef __cplusplus
