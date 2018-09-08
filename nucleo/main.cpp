f832f2b3488cacebe6fe6951d2218525ac49b74c
colinporth <colinporth@users.noreply.github.com>
2018-09-08 14:59:14 +0100

    thread t

--- nucleo/main.cpp
+++ nucleo/main.cpp
@@ -26,13 +26,77 @@ ADC_HandleTypeDef AdcHandle;
 //void DMA2_Stream0_IRQHandler() { HAL_DMA_IRQHandler (AdcHandle.DMA_Handle); }
 
 //{{{
-void adcInit() {
+void uiThread (void* arg) {
+
+  cPointF centre = cPointF (160.f, 240.f);
+  float radius = 20.f;
+  const float maxRadius = 160.f;
+
+  lcd->tftInit();
+  lcd->display (70);
+
+  while (true) {
+    //if (lcd->isChanged() || (lcd->getPresentTime() >= 1000)) {
+    if (true) {
+      lcd->start();
+      lcd->clear (kBlack);
+      lcd->setShowInfo (BSP_PB_GetState (BUTTON_KEY) == 0);
+      lcd->drawInfo();
+      //{{{  get clock
+      float hourA;
+      float minuteA;
+      float secondA;
+      float subSecondA;
+      rtc->getClockAngles (hourA, minuteA, secondA, subSecondA);
+      //}}}
+      //{{{  render clock
+      int steps = 64;
+      float width = 4.f;
+      lcd->aEllipse (centre, cPointF(radius-width, radius), steps);
+      lcd->aRender (sRgba (128,128,128, 192), false);
+      lcd->aEllipseOutline (centre, cPointF(radius, radius), width, steps);
+      lcd->aRender (sRgba (180,180,0, 255), false);
+
+      float handWidth = radius > 60.f ? radius / 20.f : 3.f;
+      float hourR = radius * 0.75f;
+      lcd->aPointedLine (centre, centre + cPointF (hourR * sin (hourA), hourR * cos (hourA)), handWidth);
+      float minuteR = radius * 0.9f;
+      lcd->aPointedLine (centre, centre + cPointF (minuteR * sin (minuteA), minuteR * cos (minuteA)), handWidth);
+      lcd->aRender (kWhite);
+
+      float secondR = radius * 0.95f;
+      lcd->aPointedLine (centre, centre + cPointF (secondR * sin (secondA), secondR * cos (secondA)), handWidth);
+      lcd->aRender (sRgba (255,0,0, 180));
+
+      float subSecondR = radius * 0.95f;
+      lcd->aPointedLine (centre, centre + cPointF (minuteR * sin (subSecondA), minuteR * cos (subSecondA)), 3.f);
+      lcd->aRender (sRgba (255,255,0, 128));
+      //}}}
+      mTraceVec.draw (lcd, 40, 400);
+
+      lcd->cLcd::text (kWhite, 30, rtc->getClockTimeDateString(), cRect (0, 426, 320, 480));
+      lcd->present();
+
+      if (radius < maxRadius) {
+        radius *= 1.1f;
+        if (radius > maxRadius)
+          radius = maxRadius;
+        lcd->change();
+        }
+      }
+    vTaskDelay (10);
+    }
+  }
+//}}}
+//{{{
+void adcThread (void* arg) {
 // pa0  5v
 // pa1  yDown
 // pa2  yUp
 // pa3  xRight
 // pa4  xLeft
 
+  //{{{  adc init
   __HAL_RCC_ADC_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_ADC_CONFIG (RCC_ADCCLKSOURCE_SYSCLK);
@@ -81,8 +145,8 @@ void adcInit() {
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init (GPIOA, &GPIO_InitStruct);
-  HAL_GPIO_WritePin (GPIOA, GPIO_PIN_3, GPIO_PIN_SET);   // resetHi
   HAL_GPIO_WritePin (GPIOA, GPIO_PIN_4, GPIO_PIN_RESET); // resetLo
+  HAL_GPIO_WritePin (GPIOA, GPIO_PIN_3, GPIO_PIN_SET);   // resetHi
   //}}}
   //{{{  y
   //// xRight - adc
@@ -143,90 +207,19 @@ void adcInit() {
 
   if (HAL_ADC_Start (&AdcHandle) != HAL_OK)
     printf ("HAL_ADC_Start failed\n");
-  }
-//}}}
+  //}}}
 
-//{{{
-void uiThread (void* arg) {
-
-  int avVal = 0;
   int convertedValue = 0;
-
-  cPointF centre = cPointF (160.f, 240.f);
-  float radius = 20.f;
-  const float maxRadius = 160.f;
-
-  lcd->tftInit();
-  lcd->display (70);
-
   while (true) {
-    if (HAL_ADC_PollForConversion (&AdcHandle, 10) != HAL_OK)
-      printf ("HAL_ADC_PollForConversion failed\n");
-    else {
+    if (!HAL_ADC_PollForConversion (&AdcHandle, 100)) {
       convertedValue = HAL_ADC_GetValue (&AdcHandle);
-    mTraceVec.addSample (0, convertedValue);
-
-    //if (lcd->isChanged() || (lcd->getPresentTime() >= 1000)) {
-    if (true) {
-      lcd->start();
-      lcd->clear (kBlack);
-      lcd->setShowInfo (BSP_PB_GetState (BUTTON_KEY) == 0);
-      lcd->drawInfo();
-      //{{{  get clock
-      float hourA;
-      float minuteA;
-      float secondA;
-      float subSecondA;
-      rtc->getClockAngles (hourA, minuteA, secondA, subSecondA);
-      //}}}
-      //{{{  render clock
-      int steps = 64;
-      float width = 4.f;
-      lcd->aEllipse (centre, cPointF(radius-width, radius), steps);
-      lcd->aRender (sRgba (128,128,128, 192), false);
-      lcd->aEllipseOutline (centre, cPointF(radius, radius), width, steps);
-      lcd->aRender (sRgba (180,180,0, 255), false);
-
-      float handWidth = radius > 60.f ? radius / 20.f : 3.f;
-      float hourR = radius * 0.75f;
-      lcd->aPointedLine (centre, centre + cPointF (hourR * sin (hourA), hourR * cos (hourA)), handWidth);
-      float minuteR = radius * 0.9f;
-      lcd->aPointedLine (centre, centre + cPointF (minuteR * sin (minuteA), minuteR * cos (minuteA)), handWidth);
-      lcd->aRender (kWhite);
-
-      float secondR = radius * 0.95f;
-      lcd->aPointedLine (centre, centre + cPointF (secondR * sin (secondA), secondR * cos (secondA)), handWidth);
-      lcd->aRender (sRgba (255,0,0, 180));
-
-      float subSecondR = radius * 0.95f;
-      lcd->aPointedLine (centre, centre + cPointF (minuteR * sin (subSecondA), minuteR * cos (subSecondA)), 3.f);
-      lcd->aRender (sRgba (255,255,0, 128));
-      //}}}
-      mTraceVec.draw (lcd, 40, 400);
-
-      lcd->cLcd::text (kWhite, 30, rtc->getClockTimeDateString(), cRect (0, 426, 320, 480));
-      lcd->cLcd::text (kWhite, 26, dec(convertedValue), cRect (0, 20, 320, 46));
-      lcd->present();
-
-      if (radius < maxRadius) {
-        radius *= 1.1f;
-        if (radius > maxRadius)
-          radius = maxRadius;
-        lcd->change();
-        }
+      mTraceVec.addSample (0, convertedValue);
       }
-    else
-      vTaskDelay (1);
-
-      //if (avVal == 0)
-      //  avVal = ConvertedValue;
-      //lse
-      //  avVal = (avVal*50 + ConvertedValue) / 51;
-      //float kScale = ((3.3f * (39.f + 27.f) / 39.f) / 4096.f) * 1000;
-      //float kScale = (3.3f / 4096.f) * 1000;
-      //float kScale = 3.f * (3.3f / 4096.f) * 1000;
-      }
+    vTaskDelay (5);
     }
+  //float kScale = ((3.3f * (39.f + 27.f) / 39.f) / 4096.f) * 1000;
+  //float kScale = (3.3f / 4096.f) * 1000;
+  //float kScale = 3.f * (3.3f / 4096.f) * 1000;
   }
 //}}}
 //{{{
@@ -300,11 +293,14 @@ int main() {
   lcd = new cLcd();
   lcd->init (kHello);
 
-  adcInit();
   mTraceVec.addTrace (320, 1, 1);
 
   TaskHandle_t uiHandle;
   xTaskCreate ((TaskFunction_t)uiThread, "ui", 4096, 0, 4, &uiHandle);
+
+  TaskHandle_t adcHandle;
+  xTaskCreate ((TaskFunction_t)adcThread, "adc", 1024, 0, 3, &adcHandle);
+
   vTaskStartScheduler();
 
   return 0;
